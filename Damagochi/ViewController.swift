f6fb32b371e3f6fcdc7e7a270a499d06e4d88a6f
Yoon-hub <kong4170@naver.com>
2022-07-22 18:48:16 +0900

    Start Project

---
+++ Damagochi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Damagochi
+//
+//  Created by 최윤제 on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
